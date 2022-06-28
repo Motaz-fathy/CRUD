19fe77ca05be11a4c5bd6d748aa134166b1d6a28
Motaz-fathy <wazaxxp@gmail.com>
2022-06-28 18:05:50 +0200

    implement update and delete operation on server

--- server/controllers/countries.js
+++ server/controllers/countries.js
@@ -18,21 +18,47 @@ countries.Add = async (req, res) => {
     const exist = await Countrie.findOne({ name });
     if (exist) {
       res.status(401).json({ message: "already exist C" });
-    } 
-    else {
-        const countrie = await Countrie.create({
-            domains,
-            country,
-            name,
-            code,
-            link
-          });
-          res.status(200).json(countrie);
+    } else {
+      const countrie = await Countrie.create({
+        domains,
+        country,
+        name,
+        code,
+        link
+      });
+      res.status(200).json(countrie);
     }
-    
   } catch (error) {
     res.status(400).json({ message: "Invalid Countrie data" });
   }
 };
 
+countries.Update = async (req, res) => {
+  const { domains, country, name, code, link } = req.body;
+  const countryOne = await Countrie.findById( req.params.id );
+  console.log(countryOne)
+  if(countryOne) {
+    countryOne.domains = domains || countryOne.domains
+    countryOne.country = country || countryOne.country
+    countryOne.name = name || countryOne.name
+    countryOne.code = code || countryOne.code
+    countryOne.link = link || countryOne.link
+    const updatedcountry = await countryOne.save();
+      res.status(200).json(updatedcountry);
+  }
+  else{
+    res.status(400).json({message : "this country not found "})
+  }
+
+};
+
+countries.Delete = async (req , res ) => {
+ const country = await Countrie.findOneAndDelete(req.params.id)
+ if(country) {
+    res.status(200).json({message : " deleted successed "})
+ } else {
+    res.status(400).json({message : " country not found  "})
+
+ }
+}
 module.exports = countries;
--- server/routes/v1.js
+++ server/routes/v1.js
@@ -3,7 +3,8 @@ const countries = require('../controllers/countries')
 const router = express.Router()
 
 router.post('/' , countries.Add)
-router.post('/:id' , countries.Update)
+router.put('/:id' , countries.Update)
+router.delete('/:id' , countries.Delete)
 router.get('/' , countries.GetAll)
 
 module.exports = router 
